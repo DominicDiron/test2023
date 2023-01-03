adca3ea4d4eeac7241a92e1d0b76f5e3b4f48d9c
DominicDiron <dirondemel99@gmail.com>
2023-01-03 14:34:51 +0530

    sub file

---
+++ sub.java
@@ -0,0 +1,11 @@
+class sub
+{
+    public static void main(String args[])
+    {
+        int a =1;
+        int b=2;
+        int c = 4;
+        System.out.println("sub = ");
+        System.out.println(a-b);
+    }
+}
