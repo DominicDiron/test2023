7eb7af18398d3754fbab49e6ee160c8172468d35
DominicDiron <107987242+DominicDiron@users.noreply.github.com>
2023-01-21 10:13:41 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -2,9 +2,8 @@ class sub
 {
     public static void main(String args[])
     {
-        int a =1;
-        int b=2;
-        int c = 4;
+        int a = 1;
+        int b = 2;
         System.out.println("sub = ");
         System.out.println(a-b);
     }
