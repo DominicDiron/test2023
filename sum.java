77cb35ba0a45691a4b25d36e1943414a5c1be8b3
DominicDiron <dirondemel99@gmail.com>
2023-01-21 10:12:27 +0530

    sum file modified

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ class sum
         int a =1;
         int b=2;
         int c = 4;
-        System.out.println("sum = ");
-        System.out.println(a+b);//sumation
+        System.out.print("sum = ");
+        System.out.println(a+b);
     }
 }
